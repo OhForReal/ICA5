b79871674e40ec75678466d306ec566497591ca8
OhForReal <jianguo@smu.edu>
2018-11-11 21:22:08 -0600

    add decisiontree and gb model

--- export_coreml.py
+++ export_coreml.py
@@ -15,6 +15,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 import numpy as np
 
@@ -22,7 +24,7 @@ import numpy as np
 import coremltools
 
 
-dsid = 2
+dsid = 5
 client  = MongoClient(serverSelectionTimeoutMS=50)
 db = client.sklearndatabase
 
@@ -42,12 +44,18 @@ clf = RandomForestClassifier(n_estimators=150)
 clf_svm = SVC()
 clf_pipe = Pipeline([("SCL", StandardScaler()),
 	("SVC",SVC())])
+clf_knn = KNeighborsClassifier(n_neighbors=3)
+clf_dt = DecisionTreeClassifier(random_state=0)
+clf_gb = GradientBoostingClassifier()
 
 print("Training Model", clf)
 
 clf.fit(X,y)
 clf_svm.fit(X,y)
 clf_pipe.fit(X,y)
+clf_knn.fit(X,y)
+clf_dt.fit(X,y)
+clf_gb.fit(X,y)
 
 print("Exporting to CoreML")
 
@@ -58,17 +66,36 @@ coreml_model = coremltools.converters.sklearn.convert(
 coreml_model.save('RandomForestAccel.mlmodel')
 
 
-coreml_model = coremltools.converters.sklearn.convert(
-	clf_svm) 
+#coreml_model = coremltools.converters.sklearn.convert(
+#	clf_svm) 
 
 # save out as a file
-coreml_model.save('SVMAccel.mlmodel')
+#coreml_model.save('SVMAccel.mlmodel')
 
-coreml_model = coremltools.converters.sklearn.convert(
-	clf_pipe) 
+#coreml_model = coremltools.converters.sklearn.convert(
+#	clf_pipe) 
 
 # save out as a file
-coreml_model.save('PipeAccel.mlmodel')
+#coreml_model.save('PipeAccel.mlmodel')
+
+
+#coreml_model = coremltools.converters.sklearn.convert(
+	#clf_knn) 
+
+# save out as a file
+#coreml_model.save('KnnAccel.mlmodel')
+
+coreml_model = coremltools.converters.sklearn.convert(
+	clf_dt) 
+
+# save out as a file
+coreml_model.save('DecisionTreeAccel.mlmodel')
+
+coreml_model = coremltools.converters.sklearn.convert(
+	clf_gb) 
+
+# save out as a file
+coreml_model.save('GradientBoostingAccel.mlmodel')
  
 
 # save out as a file
